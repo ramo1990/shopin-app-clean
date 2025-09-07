c50a2dcbc564dc54fe0be303c99e86620df9fca4
ramo1990 <abudul917@gmail.com>
2025-09-07 02:33:27 +0200

    add subcategory

--- backend/tags/admin.py
+++ backend/tags/admin.py
@@ -5,4 +5,6 @@ from .models import *
 # Register your models here.
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent')
+    list_filter = ('parent',)
     prepopulated_fields = {"slug": ("name",)}
--- backend/tags/serializers.py
+++ backend/tags/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 from .models import *
 
+
+# subcategory
+class SubCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name', 'slug', 'image']
+
 # Tags
 class TagSerializer(serializers.ModelSerializer):
     image = serializers.ImageField()
+    subcategories = SubCategorySerializer(many=True, read_only=True)
 
     class Meta:
         model = Tag
-        fields = ['id', 'name', 'image', 'slug']
+        fields = ['id', 'name', 'image', 'slug', 'parent', 'subcategories']
